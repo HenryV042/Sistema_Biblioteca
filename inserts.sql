b6b1542cb1d3ffa7de91f76479018a2c2cc4cfa6
HenryV042 <111819715+HenryV042@users.noreply.github.com>
2024-08-06 16:40:50 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,8 +1,18 @@
 INSERT INTO turma (nome_identificacao, curso, ano_inicio, ano_conclusao, serie)
 VALUES
-  ('Turma A', 'Informática', 2020, 2023, 1),
-  ('Turma B', 'Administração', 2021, 2024, 2),
-  ('Turma C', 'Engenharia', 2022, 2025, 3);
+  ('1º A - Enfermagem', 'Enfermagem', 2024, 2026, 1),
+  ('1º B - Informática', 'Informática', 2024, 2026, 1),
+  ('1º C - Comércio', 'Comércio', 2024, 2026, 1),
+  ('1º D - Administração', 'Administração', 2024, 2026, 1),
+  ('2º A - Enfermagem', 'Enfermagem', 2023, 2025, 1),
+  ('2º B - Informática', 'Informática', 2023, 2025, 1),
+  ('2º C - Comércio', 'Comércio', 2023, 2025, 1),
+  ('2º D - Administração', 'Administração', 2023, 2025, 1);
+  ('3º A - Enfermagem', 'Enfermagem', 2022, 2024, 1),
+  ('3º B - Informática', 'Informática', 2022, 2024, 1),
+  ('3º C - Comércio', 'Comércio', 2022, 2024, 1),
+  ('3º D - Administração', 'Administração', 2022, 2024, 1);
+
 
 INSERT INTO aluno (numero, matricula, cpf, nome_completo, sala_identificacao, curso, serie)
 VALUES
@@ -21,3 +31,4 @@ VALUES
   ((SELECT id FROM aluno WHERE matricula = '123456'), '123456', 'Livro de Informática', '123456', 'Informática', '1', '2023-03-01 10:00:00', '2023-03-15 10:00:00', 'Bibliotecário 1', 'Emprestado'),
   ((SELECT id FROM aluno WHERE matricula = '234567'), '234567', 'Livro de Administração', '234567', 'Administração', '2', '2023-04-01 10:00:00', '2023-04-15 10:00:00', 'Bibliotecário 2', 'Emprestado'),
   ((SELECT id FROM aluno WHERE matricula = '345678'), '345678', 'Livro de Engenharia', '345678', 'Engenharia', '3', '2023-05-01 10:00:00', '2023-05-15 10:00:00', 'Bibliotecário 3', 'Emprestado');
+
