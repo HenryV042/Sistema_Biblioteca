d2e095d1af854f1c899d5ba21283d63a154aea6d
GustavoHenrique <gustavotucano@gmail.com>
2024-08-06 13:54:22 -0300

    alteração no html/css. adição de icones; alteração de tag na area login.php; adição de action e method no form; adição de imagens; adição de stilização no css

--- PageUser/Login/login.php
+++ PageUser/Login/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <title>Tela Login</title>
     <link rel="stylesheet" href="./style.css">
 </head>
@@ -21,16 +22,18 @@
                 <div class="restrito">
                   <span class="ClasseTexto">Acesso restrito</span>
                 </div>
-                <form>
+                <form action="" method="post">
                     <div class="mb-3">
                       <label for="exampleInputEmail1" class="form-label"></label>
+                      <img class="iconeUsuario" src="img/do-utilizador.png" alt="" >
                       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Usuario/Email">
                       
                     <div class="mb-3">
                       <label for="exampleInputPassword1" class="form-label"></label>
+                      <img class="iconeTranca" src="img/trancar.png" alt="">
                       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
                     </div>
-                    <button type="submit" class="teubotao">Entrar</button>
+                    <button type="submit" class="BotaoDeentrar">Entrar</button>
                   </form>
             </div>
         </div>
