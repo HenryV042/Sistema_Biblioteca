5fd2803f492facda7a1804e3f5a9b81a17b06827
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2024-08-14 14:26:43 -0300

    Add files via upload

---
+++ Bibliotecario/emprestimos/info/script.js
@@ -0,0 +1,8 @@
+function abrirMenu(){
+    document.getElementById("menu-Oculto").style.width="400px";
+    document.getElementById("principal").style.marginLeft="0px";
+}
+function fecharMenu(){
+    document.getElementById("menu-Oculto").style.width="0vw";
+    document.getElementById("principal").style.marginLeft="0vw";
+}
