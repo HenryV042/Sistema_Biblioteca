a46aa716f480698b9725d519865a6c7c62b9d291
Paulo Henrique <117217497+phgomes40@users.noreply.github.com>
2024-08-19 14:46:04 -0300

    Add files via upload

---
+++ Bibliotecario/pdf/gerar_pdf.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.2/html2pdf.bundle.min.js" integrity="sha512-MpDFIChbcXl2QgipQrt1VcPHMldRILetapBl5MPCA9Y8r7qvlwx1/Mc9hNTzY+kS5kX6PdoDq41ws1HiVNLdZA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="pdf.js" defer></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.min.js"></script> -->
+</head>
+<body>
+    <!-- Conteúdo do PDF -->
+    <div id="content">
+        <h1>Futuro gerador de PDF</h1>
+        <p>Conteúdo do pdf</p>
+    </div>
+    <button id="generate_pdf">Gerar</button>
+</body>
+</html>
