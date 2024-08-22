e77ae0e26527bc8b57f521da70d7731550b67a38
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2024-08-22 13:21:38 -0300

    Delete Update status.sql

--- Update status.sql
+++
@@ -1,5 +0,0 @@
--- Adicionar coluna tipo à tabela bibliotecario
-ALTER TABLE bibliotecario ADD tipo ENUM('bibliotecario', 'aluno') NOT NULL DEFAULT 'bibliotecario';
-
--- Adicionar coluna tipo à tabela aluno (opcional, se você quiser uma coluna separada)
-ALTER TABLE aluno ADD tipo ENUM('aluno') NOT NULL DEFAULT 'aluno';
